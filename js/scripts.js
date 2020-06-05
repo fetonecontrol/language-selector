13ba3d8b82d28f366d2ec948f05457d6915951d1
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-05 11:52:03 -0700

    add .length function to user input to measure string length

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $("#lang-select").submit(function(event) {
     const food = $("#food").val();
-
-    $("#output").text(food);
+   // const c# = "C#"
+   // const dotNet = ".Net"
+   // const javaScript = "Java Script"
+    $("#output").text(food.length);
     $("#hide-out").show();
 
     event.preventDefault();
