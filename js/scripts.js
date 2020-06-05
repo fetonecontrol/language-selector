74f5aa05be9f9fa83c262d395816fe332bb98bf3
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-05 11:45:43 -0700

    fix output not displaying correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("#lang-select").submit(function(event) {
     const food = $("#food").val();
-    
-    $("#food").text(food);
+
+    $("#output").text(food);
     $("#hide-out").show();
 
     event.preventDefault();
