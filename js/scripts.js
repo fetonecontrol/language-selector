605d264f919c632cf08309716598ca35efc5f93d
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-05 11:12:26 -0700

    remove other types of input while trouble shooting output display problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
   $("#lang-select").submit(function(event) {
     const food = $("#food").val();
-    const animal = $("#animal").val();
-    const color = $("#color").val();
-    const born = $("born").val();
-    const preference = $("prefence").val();
-
+    
     $("#food").text(food);
-    $(".hide-out").show();
+    $("#hide-out").show();
 
     event.preventDefault();
   });
