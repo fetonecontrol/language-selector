0a9db35ee18de40a40447e87b185fdb3252c7f94
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-05 12:50:58 -0700

    add additional conditional statements

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
   $("#lang-select").submit(function(event) {
     const food = $("#food").val();
     const animal = $("#animal").val();
-   // const c# = "C#"
+    const preLanguage = parseInt($("#preference").val());
     const dotNet = ".Net"
-   // const javaScript = "Java Script"
+    const javaScript = "Java Script"
+    const seeSharp = "C#"
 
-    if (food.length + animal.length >= 0){
+    if (food.length + animal.length + preLanguage === 1){
       $("#output").text(dotNet);
+    } else if (food.length + animal.length + preLanguage <= 10000) {
+      $("#output").text(javaScript);
+    } else if (food.length + animal.length + preLanguage === 0) {
+      $("#output").text(seeSharp);
     }
     $("#hide-out").show();
 
