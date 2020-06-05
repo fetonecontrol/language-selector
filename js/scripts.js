c385e113d72ca512c5a5a2d7056fab982aa70030
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-05 12:34:00 -0700

    add initial if statement to test functionality of .length function on strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   $("#lang-select").submit(function(event) {
     const food = $("#food").val();
+    const animal = $("#animal").val();
    // const c# = "C#"
-   // const dotNet = ".Net"
+    const dotNet = ".Net"
    // const javaScript = "Java Script"
-    $("#output").text(food.length);
+
+    if (food.length + animal.length >= 0){
+      $("#output").text(dotNet);
+    }
     $("#hide-out").show();
 
     event.preventDefault();
