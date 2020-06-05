b335a8a686d36b62a1f58257f1aa320adf24ec17
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-05 13:05:11 -0700

    add additional forms now that functionality is verified

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
   $("#lang-select").submit(function(event) {
     const food = $("#food").val();
     const animal = $("#animal").val();
+    const color = $("#color").val();
+    const dob = $("#born").val();
     const preLanguage = parseInt($("#preference").val());
     const dotNet = ".Net"
     const javaScript = "Java Script"
     const seeSharp = "C#"
 
-    if (food.length + animal.length + preLanguage === 1){
+    if (food.length + animal.length + preLanguage <= 25) {
       $("#output").text(dotNet);
     } else if (food.length + animal.length + preLanguage <= 10000) {
       $("#output").text(javaScript);
